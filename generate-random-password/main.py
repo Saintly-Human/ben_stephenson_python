a19e837d9f00e16a38b6d6c74f7aa433de3aef8d
Saint <lenovothinkbook13@gmail.com>
2026-08-21 18:04:33 +0500

    fixes

--- generate-random-password/main.py
+++ generate-random-password/main.py
@@ -24,7 +24,7 @@ def generate_password(file_path: str) -> None:
         print(f"Input/Output Error: {e}")
 def main() -> None:
     if len(sys.argv) < 2:
-        print("Usage: python main.py <filename>")
+        print("Usage: python main.py <file_name>")
         return
     generate_password(sys.argv[1])
 if __name__ == "__main__":
